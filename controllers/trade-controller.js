d8181f39a81c621517bcaeb413ef1b44c387c1a8
vskcodesoftic <vegisiva@codesoftic.com>
2021-06-01 10:50:17 +0530

    trade request succfully completed and tested

--- controllers/trade-controller.js
+++ controllers/trade-controller.js
@@ -20,176 +20,176 @@ const HttpError = require("../middleware/http-error");
 
 const { v1: uuid } = require("uuid");
 
-const sendTradeRequest = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(errors);
-    const error = new HttpError(
-      "invalid input are passed,please pass valid data",
-      422
-    );
-    return next(error);
-  }
+// const sendTradeRequest = async (req, res, next) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     console.log(errors);
+//     const error = new HttpError(
+//       "invalid input are passed,please pass valid data",
+//       422
+//     );
+//     return next(error);
+//   }
 
-  let message 
-  //user who has loggedin
-  const loggedUser = req.userData;
-  const fcmTokenOfLoggedUser = req.userData.fcmToken;
+//   let message 
+//   //user who has loggedin
+//   const loggedUser = req.userData;
+//   const fcmTokenOfLoggedUser = req.userData.fcmToken;
  
-   console.log(fcmTokenOfLoggedUser)
+//    console.log(fcmTokenOfLoggedUser)
    
-  const {
-    userproductId,
-    offeredProductId,
-    senderId,
-    senderName,
-    senderNationality,
-    productsOffered,
-  } = req.body;
+//   const {
+//     userproductId,
+//     offeredProductId,
+//     senderId,
+//     senderName,
+//     senderNationality,
+//     productsOffered,
+//   } = req.body;
 
-  //let productIds = []
-  let productIds = await {offeredProductId}
+//   //let productIds = []
+//   let productIds = await {offeredProductId}
   
 
 
-   //ProposedProductIdbyLoggedUser =  productIds.push(`${offeredProductId}`);;
+//    //ProposedProductIdbyLoggedUser =  productIds.push(`${offeredProductId}`);;
 
-   ProposedProductIdbyLoggedUser = await productIds;
-
-  
-   const productId = userproductId; //(objectidofproduct)
-   //the userProduct to whom he wanna offer trade (single id of product from slider)
-
-
-  // finding the productId of provided
-  let product;
-
-  try {
-    product = await Product.findById(productId);
-  } catch (err) {
-    const error = new HttpError(
-      "Something went wrong, could not find a product.",
-      500
-    );
-    return next(error);
-  }
-
-  if (!product) {
-    const error = new HttpError(
-      "Could not find a product for the provided id.",
-      404
-    );
-    return next(error);
-  }
-
-  const creatorIdofUser = product.creator;
-
-//obtained creatorId from sliderProduct
-
-  let user;
-  try {
-    user = await User.findById(creatorIdofUser);
-  } catch (err) {
-    const error = new HttpError(
-      "Something went wrong, could not find a user.",
-      500
-    );
-    return next(error);
-  }
-
-  if (!user) {
-    const error = new HttpError(
-      "Could not find a user for the provided id.",
-      404
-    );
-    return next(error);
-  }
-
-
-
-  const PrdEmail = await user.email
-  const PrdCountry = await user.country
-  const PrdNationality = await user.nationality
-  const PrdNickname = await user.nickname
-
-  const FcmTokenofBuser = await user.fcmToken;
-
-
-   console.log("buser" +FcmTokenofBuser);
-
-  // offered Products 
-  let offrdProducts  = await offeredProductId.pids
-
-  for (const productOffrd in offrdProducts) {
-   // console.log(`${prduct}: ${ds[prduct]}`);
-    let products;
-    try {
-      products = await Product.findById(`${offrdProducts[productOffrd]}`);
-    } catch (err) {
-      const error = new HttpError(
-        "Something went wrong, could not find a product.",
-        500
-      );
-      return next(error);
-    }
-  
-    if (!products) {
-      const error = new HttpError(
-        "Could not find a product for the provided id.",
-        404
-      );
-      return next(error);
-    }
-    const productTitle = products.title;
-    const productDescription = products.description;
-    const productQuantity = products.quantity;
-
-    console.log("title :" + productTitle +" ,"+"desc :" +productDescription +" ,"+"quantity   :" + productQuantity)
-
-     message = `"your message : ${productTitle}, ${productDescription}"`
-    console.log(message)
-
+//    ProposedProductIdbyLoggedUser = await productIds;
 
   
-  }
+//    const productId = userproductId; //(objectidofproduct)
+//    //the userProduct to whom he wanna offer trade (single id of product from slider)
 
-  //sendingNotification
-  const createdNotification = new Notification({
-    message,
-    creator: creatorIdofUser,
-    productsOffered : [offrdProducts],
-    userproductId ,
-    productOfferedEmail  :  PrdEmail,
-    productOfferedNationality :   PrdNationality,
-    productOfferedCountry : PrdCountry,
-    productOfferedNickname : PrdNickname,
-    userEmail : loggedUser.email,
-    country: PrdCountry,
 
-  });
+//   // finding the productId of provided
+//   let product;
 
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await createdNotification.save({ session: sess });
-    user.notifications.push(createdNotification);
-    await user.save({ session: sess });
-    await sess.commitTransaction();
+//   try {
+//     product = await Product.findById(productId);
+//   } catch (err) {
+//     const error = new HttpError(
+//       "Something went wrong, could not find a product.",
+//       500
+//     );
+//     return next(error);
+//   }
+
+//   if (!product) {
+//     const error = new HttpError(
+//       "Could not find a product for the provided id.",
+//       404
+//     );
+//     return next(error);
+//   }
+
+//   const creatorIdofUser = product.creator;
+
+// //obtained creatorId from sliderProduct
+
+//   let user;
+//   try {
+//     user = await User.findById(creatorIdofUser);
+//   } catch (err) {
+//     const error = new HttpError(
+//       "Something went wrong, could not find a user.",
+//       500
+//     );
+//     return next(error);
+//   }
+
+//   if (!user) {
+//     const error = new HttpError(
+//       "Could not find a user for the provided id.",
+//       404
+//     );
+//     return next(error);
+//   }
+
+
+
+//   const PrdEmail = await user.email
+//   const PrdCountry = await user.country
+//   const PrdNationality = await user.nationality
+//   const PrdNickname = await user.nickname
+
+//   const FcmTokenofBuser = await user.fcmToken;
+
+
+//    console.log("buser" +FcmTokenofBuser);
+
+//   // offered Products 
+//   let offrdProducts  = await offeredProductId.pids
+
+//   for (const productOffrd in offrdProducts) {
+//    // console.log(`${prduct}: ${ds[prduct]}`);
+//     let products;
+//     try {
+//       products = await Product.findById(`${offrdProducts[productOffrd]}`);
+//     } catch (err) {
+//       const error = new HttpError(
+//         "Something went wrong, could not find a product.",
+//         500
+//       );
+//       return next(error);
+//     }
+  
+//     if (!products) {
+//       const error = new HttpError(
+//         "Could not find a product for the provided id.",
+//         404
+//       );
+//       return next(error);
+//     }
+//     const productTitle = products.title;
+//     const productDescription = products.description;
+//     const productQuantity = products.quantity;
+
+//     console.log("title :" + productTitle +" ,"+"desc :" +productDescription +" ,"+"quantity   :" + productQuantity)
+
+//      message = `"your message : ${productTitle}, ${productDescription}"`
+//     console.log(message)
+
+
+  
+//   }
+
+//   //sendingNotification
+//   const createdNotification = new Notification({
+//     message,
+//     creator: creatorIdofUser,
+//     productsOffered : [offrdProducts],
+//     userproductId ,
+//     productOfferedEmail  :  PrdEmail,
+//     productOfferedNationality :   PrdNationality,
+//     productOfferedCountry : PrdCountry,
+//     productOfferedNickname : PrdNickname,
+//     userEmail : loggedUser.email,
+//     country: PrdCountry,
+
+//   });
+
+//   try {
+//     const sess = await mongoose.startSession();
+//     sess.startTransaction();
+//     await createdNotification.save({ session: sess });
+//     user.notifications.push(createdNotification);
+//     await user.save({ session: sess });
+//     await sess.commitTransaction();
  
 
 
 
-  } catch (err) {
-      console.log(err)
-    const error = new HttpError(
-      'Creating notfication failed, please try again.',
-      500
-    );
-    return next(error);
-  }
+//   } catch (err) {
+//       console.log(err)
+//     const error = new HttpError(
+//       'Creating notfication failed, please try again.',
+//       500
+//     );
+//     return next(error);
+//   }
    
-  res.json({  LoggedUserEmail: loggedUser.email , createdNotification : createdNotification });
-};
+//   res.json({  LoggedUserEmail: loggedUser.email , createdNotification : createdNotification });
+// };
 
 
 
@@ -237,16 +237,17 @@ const acceptTrade = async (req, res ,next) => {
     return next(error);
   }
 
-
+   // user single Products
+      const userProduct  = await  notification.userproductId;
    // offered Products 
    let offrdProducts  = await  notification.productsOffered;
      let ProductIds=[];
+     ProductIds.push(`${userProduct}`);
+
     for (const [key, value] of Object.entries(offrdProducts[0])) {
       ProductIds.push(`${value}`)
           
-      //UPDATINGproduct
-
-//updating status of products
+//updating status of offered products
 let product;
 try {
   product = await Product.findById(`${value}`);
@@ -261,6 +262,8 @@ try {
 
 //setting product expiry 15days
 product.isShow = "false";
+product.isFeatured="false";
+product.status="notConfirmed";
 product.expireToken = Date.now() + 1297000000
 
 try {
@@ -566,7 +569,7 @@ fcm.send(message, function(err, response){
 
 
 
-exports.sendTradeRequest = sendTradeRequest;
+//exports.sendTradeRequest = sendTradeRequest;
 exports.acceptTrade = acceptTrade;
 exports.confirmTradeRequest =confirmTradeRequest;
 exports.sendDualTradeNotification = sendDualTradeNotification;
