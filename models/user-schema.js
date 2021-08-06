cb27919c2c0bfbfb4dbb3b5fa6f5dbf3432356d0
vskcodesoftic <vegisiva@codesoftic.com>
2021-08-06 17:44:07 +0530

    removed phoneNumber requied

--- models/user-schema.js
+++ models/user-schema.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
     country : {type : Number, required : true},
     countryTwoLetterCode : {type : String },
     fcmToken : { type : String , default: 'dLlzacFRRAGIiE9lm7llyf:APA91bEODGAvxkjhn6GRdH2KhjOiobgETrpW0WeJ4R2WI4m4wW_SJ6G2_TJGoIlhv9sG38lsAtCpCq1TJHXG9PLFWhx0NCdgxwpA4AMXGZ7v8D_ArQV-v3FNzKpkfbw4x-XZha83YPbQ' },
-    phoneNumber : { type : Number, required : true},
+    phoneNumber : { type : Number },
     nationality : { type : String},
     country : { type : String},
     dob : { type : String },
